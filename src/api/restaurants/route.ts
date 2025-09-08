9860328f7f0c8301f6611fdac2d08fe69e2dcb9f
Kyung Lee <kyung.lee.official@outlook.com>
2025-09-08 14:27:05 +0800

    feat: add get restaurant list route

--- src/api/restaurants/route.ts
+++ src/api/restaurants/route.ts
@@ -1,5 +1,9 @@
 import { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
-import { MedusaError } from "@medusajs/framework/utils";
+import {
+	ContainerRegistrationKeys,
+	MedusaError,
+	QueryContext,
+} from "@medusajs/framework/utils";
 import { restaurantSchema } from "./validation-schemas";
 import { CreateRestaurant } from "../../modules/restaurant/types";
 import { createRestaurantWorkflow } from "../../workflows/restaurant/create-restaurant";
@@ -20,4 +24,40 @@ export async function POST(req: MedusaRequest, res: MedusaResponse) {
 	});
 
 	return res.status(200).json({ restaurant });
+}
+
+export async function GET(req: MedusaRequest, res: MedusaResponse) {
+	const { currency_code = "eur", ...queryFilters } = req.query;
+
+	const query = req.scope.resolve(ContainerRegistrationKeys.QUERY);
+
+	const { data: restaurants } = await query.graph({
+		entity: "restaurants",
+		fields: [
+			"id",
+			"handle",
+			"name",
+			"address",
+			"phone",
+			"email",
+			"image_url",
+			"is_open",
+			"products.*",
+			"products.categories.*",
+			"products.variants.*",
+			"products.variants.calculated_price.*",
+		],
+		filters: queryFilters,
+		context: {
+			products: {
+				variants: {
+					calculated_price: QueryContext({
+						currency_code,
+					}),
+				},
+			},
+		},
+	});
+
+	return res.status(200).json({ restaurants });
 }
